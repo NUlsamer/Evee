149e7857198338f8795041b6a0d5d60837068bdd
Nico Ulsamer <nicoulsamer@hotmail.de>
2019-02-09 10:49:11 +0100

    fixed build.gradle's, routes not functional atm

--- Evee/App.js
+++ Evee/App.js
@@ -6,32 +6,26 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import firebase from 'react-native-firebase';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {SwitchNavigator} from 'react-navigation'
+import React, {Component} from 'react'
+import firebase from 'react-native-firebase'
+import { Platform, StyleSheet, Text, View } from 'react-native'
+import { SwitchNavigator } from 'react-navigation'
+
 
 
 //importing screens
 import login from './src/login'
 import signup from './src/signup'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
-const App = SwitchNavigator (
+const App = SwitchNavigator(
   {
+    login,
     signup,
-    login
   },
   {
     initialRouteName: 'signup'
   }
 )
+
 
 export default App
