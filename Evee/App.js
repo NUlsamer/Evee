9c9d6e03b61c1ae65d4ff0614275c4f7a1af477e
Nico Ulsamer <nicoulsamer@hotmail.de>
2019-02-09 13:07:15 +0100

    added react-native-gesture-handler, and added dependencies to mainactivity.java

--- Evee/App.js
+++ Evee/App.js
@@ -8,5 +8,25 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 
+//import screens
+import Homescreen from './src/Homescreen'
+import login from './src/login'
+import signup from './src/signup'
 
+const AppNavigator = createStackNavigator (
+  {
+    Home: {
+      screen: Homescreen
+    },
+    login: {
+      screen: login
+    },
+    signup: {
+      screen: signup
+    }
+  }
+);
+
+export default createAppContainer(Appnavigator);
