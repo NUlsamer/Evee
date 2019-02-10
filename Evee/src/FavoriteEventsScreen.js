1d7594b7a96412684fc69146670b3723fbe86538
Nico Ulsamer <nicoulsamer@hotmail.de>
2019-02-10 19:41:58 +0100

    added screens

---
+++ Evee/src/FavoriteEventsScreen.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { View, Text, Button} from 'react-native'
+
+class FavoriteEventsScreen extends React.Component{
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Text>Choose your favorite Events</Text>
+                    <Button
+                    title="Back"
+                    onPress={() =>
+                    this.props.navigation.goBack()
+                    }
+                    />
+                    <Button
+                    title="Continue"
+                    onPress={() =>
+                    this.props.navigation.navigate('Picture')
+                    }
+                    />
+                    </View>
+        );
+    }
+}
