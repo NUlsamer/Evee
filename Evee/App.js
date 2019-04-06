775395e368aa08d40d0c80933a484ac056b9d1ea
Nico Ulsamer <nicoulsamer@hotmail.de>
2019-04-06 12:56:01 +0200

    added bottom nav at home screen

--- Evee/App.js
+++ Evee/App.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { createMaterialTopTabNavigator, createStackNavigator, createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { 
+  createMaterialTopTabNavigator,
+  createStackNavigator, 
+  createAppContainer, 
+  createSwitchNavigator,
+  createBottomTabNavigator} from 'react-navigation';
 
 //import screens
 import HomeScreen from './Screens/HomeScreen';
@@ -8,7 +13,7 @@ import Login from './Screens/LoginScreen';
 import SignUp from './Screens/SignUp/SignupScreen';
 import ProfileScreen from './Screens/ProfileScreen';
 import CreateEventScreen from './Screens/CreateEventScreen';
-import EventList from './Screens/EventList';
+import EventList from './Screens/EventListScreen';
 import FavoriteEventsScreen from './Screens/SignUp/FavoriteEventsScreen'; 
 import PictureScreen from './Screens/SignUp/PictureScreen';
 import MainChatsScreen from './Screens/Chats/MainChatsScreen';
@@ -18,13 +23,11 @@ import LandingPageScreen from './Screens/LandingPageScreen';
 import EventDetails from './Screens/Events/EventDetails';
 import EventParticipants from './Screens/Events/EventParticipants';
 
-
 const AppNavigator = createStackNavigator ( 
   {
     Home: HomeScreen,
     CreateEvent: CreateEventScreen,
     Profile: ProfileScreen,
-    Login: Login,
     EventList: EventList,
   },
   {
@@ -32,16 +35,6 @@ const AppNavigator = createStackNavigator (
   }
 );
 
-const ChatNavigator = createMaterialTopTabNavigator (
-  {
-      Chats: MainChatsScreen,
-      PopUpChats: PopUpChatsScreen,
-      GroupChats: GroupChatsScreen,
-  },
-  {
-  initialRouteName: 'Chats'
-  }
-  )
 
 const LoginNavigator = createStackNavigator (
 {
@@ -71,14 +64,33 @@ const EventNav = createStackNavigator (
 
 )
 
+const ChatNav = createMaterialTopTabNavigator (
+  {
+      Chats: MainChatsScreen,
+      PopUpChats: PopUpChatsScreen,
+      GroupChats: GroupChatsScreen,
+  },
+  {
+  initialRouteName: 'Chats'
+  }
+  )
+const HomeNav = 
+createBottomTabNavigator(
+  {
+    Home: {screen: AppNavigator},
+    List: {screen: EventList},
+    Chats: {screen: ChatNav},
+  },{
+  initialRouteName: 'Home'}
+  
+  )
 export default createAppContainer(createSwitchNavigator( 
   { 
-    SignUp: SignUpNavigator, 
-    App: AppNavigator,
-    Login: LoginNavigator,
-    Events: EventNav,
-    Chats: ChatNavigator,
-  }, 
+  Home: HomeNav,
+  SignUp: SignUpNavigator,
+Login: LoginNavigator}, 
     {
+      //hier abfrage mit ? ob user eingeloggt ist
       initialRouteName: 'SignUp'
-}));
+}),
+  );
--- Evee/Screens/HomeScreen.js
+++ Evee/Screens/HomeScreen.js
@@ -5,7 +5,8 @@ import { View, Text, Button} from 'react-native'
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
-  };  
+  };
+
   render() {
       return (
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
