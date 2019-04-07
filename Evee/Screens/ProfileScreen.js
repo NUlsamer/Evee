e97c25e9cb00398d69ebcfe9b868fdd05f4f1151
10Fi1 <frei.christina@web.de>
2019-04-07 17:37:40 +0200

    added picture in profile

--- Evee/Screens/ProfileScreen.js
+++ Evee/Screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button, Image } from 'react-native';
 import firebase from 'react-native-firebase';
 import SignUp from './SignUp/SignupScreen';
 
@@ -18,13 +18,13 @@ class ProfileScreen extends React.Component {
 //        const user = firebase.auth().currentUser;
 
         return (
-        <View>
+        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Image style={{ width: 300,  height: 320 }} source={{ uri: 'http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png' }} />
             <Text>Profilepic comes here</Text>
             <Text>Profilename: </Text>
             <Text>Emailadress: </Text>
             <Text>Age</Text>
             <Button title="Sign out" onPress={() => this.props.navigation.navigate('Home')}/>
-
         </View>
         );
     }
